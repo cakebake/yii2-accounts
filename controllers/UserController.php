e5c0248434e101b734fb84e212c8ebeac36ef465
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-07 23:25:05 +0200

    Added login action and beforeAction routine

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,12 +2,63 @@
 
 namespace cakebake\accounts\controllers;
 
+use Yii;
 use yii\web\Controller;
 
 class UserController extends Controller
 {
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        if (!parent::beforeAction($action))
+            return false;
+
+        switch ($action->id) {
+           case 'index':
+                if (Yii::$app->user->isGuest) {
+                    $this->goLogin();
+                }
+             break;
+           case 'login':
+
+             break;
+//           case 'profile':
+//
+//             break;
+//           case 'edit':
+//
+//             break;
+        }
+
+        return true;
+    }
+
+    /**
+    * The default action of this module
+    */
     public function actionIndex()
     {
         return $this->render('index');
     }
+
+    /**
+    * The login action
+    */
+    public function actionLogin()
+    {
+        return $this->render('login');
+    }
+
+    /**
+    * Redirects the browser to the login page.
+    *
+    * @return Response the current response object
+    */
+    public function goLogin()
+    {
+        $this->redirect(['/accounts/user/login']);
+    }
+
 }
