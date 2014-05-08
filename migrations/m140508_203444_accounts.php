47f4da0dd5234330fe4c02b0c7133a8300330be4
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-08 22:44:27 +0200

    Added basic user table

---
+++ migrations/m140508_203444_accounts.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Schema;
+
+class m140508_203444_accounts extends \yii\db\Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        /**
+        * CREATE user table
+        */
+        $this->createTable('{{%user}}', [
+            'id' => Schema::TYPE_PK,
+            'username' => Schema::TYPE_STRING . ' NOT NULL',
+            'email' => Schema::TYPE_STRING . ' NOT NULL',
+            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
+            'password_reset_token' => Schema::TYPE_STRING,
+            'role' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        /**
+        * DROP user table
+        */
+        $this->dropTable('{{%user}}');
+    }
+}
