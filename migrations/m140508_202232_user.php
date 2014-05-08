d7704dd53cc17b65b53458e8d7a6c3713b27ea1f
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-08 22:24:26 +0200

    Added bootstrap code for user table migration

---
+++ migrations/m140508_202232_user.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+
+class m140508_202232_user extends \yii\db\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140508_202232_user cannot be reverted.\n";
+
+        return false;
+    }
+}
