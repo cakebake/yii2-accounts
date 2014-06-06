5a2fd6e2d2554cbd35719fd1b308ed2747ba8497
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-06-07 01:24:39 +0200

    Added components

---
+++ components/AuthManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace cakebake\accounts\components;
+
+use Yii;
+
+/**
+ * @inheritdoc
+ */
+class AuthManager extends \yii\rbac\DbManager
+{
+    /**
+     * @inheritdoc
+     */
+    public $db = 'db';
+
+    /**
+     * @inheritdoc
+     */
+    public $itemTable = '{{%account_auth_item}}';
+
+    /**
+     * @inheritdoc
+     */
+    public $itemChildTable = '{{%account_auth_item_child}}';
+
+    /**
+     * @inheritdoc
+     */
+    public $assignmentTable = '{{%account_auth_assignment}}';
+
+    /**
+     * @inheritdoc
+     */
+    public $ruleTable = '{{%account_auth_rule}}';
+}
